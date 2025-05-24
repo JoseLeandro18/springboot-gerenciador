84cc01229d4bf181fbf5f7f695fdf332ba5cfa7b
José Leandro <joseleand06@gmail.com>
2025-05-24 13:48:49 -0300

    Etapa 1: Springboot conectado ao MySQL

---
+++ src/main/java/com/Joseleandro/Sistema_gerenciador_de_certificados/SistemaGerenciadorDeCertificadosApplication.java
@@ -0,0 +1,13 @@
+package com.Joseleandro.Sistema_gerenciador_de_certificados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorDeCertificadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciadorDeCertificadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Joseleandro/Sistema_gerenciador_de_certificados/SistemaGerenciadorDeCertificadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joseleandro.Sistema_gerenciador_de_certificados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorDeCertificadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
