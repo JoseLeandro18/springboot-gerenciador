d8c1f85b70acf6e35474d1e6c4d0340cc0bcf723
José Leandro <joseleand06@gmail.com>
2025-05-26 12:27:24 -0300

    Etapa 5: Remoção do SpringSecurity por motivos técnicos

--- src/main/java/com/Joseleandro/Sistema_gerenciador_de_certificados/controller/CertificadoController.java
+++ src/main/java/com/Joseleandro/Sistema_gerenciador_de_certificados/controller/CertificadoController.java
@@ -4,10 +4,7 @@ import com.Joseleandro.Sistema_gerenciador_de_certificados.model.Certificado;
 import com.Joseleandro.Sistema_gerenciador_de_certificados.repository.CertificadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
